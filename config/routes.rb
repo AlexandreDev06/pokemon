e99161df8879b126ae46e58e15ab7eafe5e1ffa7
AlexandreDev06 <alexandre.macedo1712@gmail.com>
2021-12-18 21:26:32 -0300

    criado cada rota para cada botao feito no show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
   get 'search', to: 'search#pokes'
+  post 'batles_attack', to: 'batles#attack'
+  post 'batles_defense', to: 'batles#defense'
+  post 'batles_items', to: 'batles#items'
+  post 'batles_skip', to: 'batles#skip'
 
   devise_for :trainers
   resources :teams
