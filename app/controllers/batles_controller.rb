5748c80bd6b8736b91629934448ffbad9cab327a
AlexandreDev06 <alexandre.macedo1712@gmail.com>
2021-12-18 21:25:03 -0300

    adicionado metodos e suas views

--- app/controllers/batles_controller.rb
+++ app/controllers/batles_controller.rb
@@ -1,9 +1,23 @@
 class BatlesController < ApplicationController
+  before_action :set_poke, only: %i[attack defense items skip show]
+
   def index
     @teams = current_trainer.teams
   end
 
-  def show
+  def show; end
+
+  def attack; end
+
+  def defense; end
+
+  def items; end
+
+  def skip; end
+
+  private
+
+  def set_poke
     @poke = Poke.first
   end
 end
